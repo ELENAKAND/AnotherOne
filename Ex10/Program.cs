4e98bd045234f638c893c37e6da380b5e07c75bd
ELENA <elenakand@icloud.com>
2022-06-24 23:31:50 +0300

    Квадрат числа

---
+++ Ex10/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+Вход число, выход квадрат числа
+*/
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+int result = number * number;
+Console.WriteLine(result);

62d1c0db4b661c778d952228114925478f5dbba9
ELENA <elenakand@icloud.com>
2022-06-24 16:01:02 +0300

    First using if/else

---
+++ Ex06/Program.cs
@@ -0,0 +1,12 @@
+﻿// Using if/else 
+Console.WriteLine("Введите свое имя: ");
+string username = Console.ReadLine();
+  if(username == "Маша")
+  {
+      Console.WriteLine("Ура, это Маша!");
+  }
+  else
+  {
+      Console.WriteLine("Привет, ");
+      Console.WriteLine(username);
+  }

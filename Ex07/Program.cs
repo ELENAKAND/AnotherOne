e454ecaf72ebc709888a0d543b3ecdeb9d072f02
ELENA <elenakand@icloud.com>
2022-06-24 16:54:47 +0300

    Max number

---
+++ Ex07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Finding max among certain numbers 
+int a = 3;
+int b = 5;
+int c = 2;
+int d = 1;
+int e = 8; 
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);

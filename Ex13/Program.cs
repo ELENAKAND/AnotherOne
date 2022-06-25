2c3f5dd574f9ef4dea7e1e0d7fa055ba34de3ae2
ELENA <elenakand@icloud.com>
2022-06-25 13:03:28 +0300

    Вывод значений [-N;N]

---
+++ Ex13/Program.cs
@@ -0,0 +1,9 @@
+﻿// На входе число N - на выходе значения от -N до N 
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number1 = int.Parse(numberStr); //спарсили стринг пользователя в инт 
+int number2 = -number1; //присвоили отрицательное значение второму числу 
+for (int i = number2; i <= number1; i++)
+{
+    Console.WriteLine(i);
+}

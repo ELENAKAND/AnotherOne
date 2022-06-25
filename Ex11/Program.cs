18df27a9a9e5429324da28544606db93a7bc07fc
ELENA <elenakand@icloud.com>
2022-06-25 10:20:14 +0300

    Исправлены ошибки в вычислении квадрата числа

---
+++ Ex11/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Вход 2 числа, первое число является квадратом второго (да/нет) 
+ Console.WriteLine("Введите первое число");
+ string numberStrFirst = Console.ReadLine();
+ int numberFirst = int.Parse(numberStrFirst);
+ Console.WriteLine("Введите второе число");
+ string numberStrSecond = Console.ReadLine(); //считывание стринговой переменной 
+ int numberSecond = int.Parse(numberStrSecond); //парсим перемен стринг в перем инт
+ if (numberFirst == numberSecond * numberSecond)
+ {
+    Console.WriteLine("Да");
+ }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+ 

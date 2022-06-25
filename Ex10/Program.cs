9a01b61e668915ce498f6d7f9b2efce7dff59f1d
ELENA <elenakand@icloud.com>
2022-06-25 09:02:11 +0300

    Finding the square of a number

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -5,4 +5,5 @@ Console.WriteLine("Введите число");
 string numberStr = Console.ReadLine();
 int number = int.Parse(numberStr);
 int result = number * number;
-Console.WriteLine(result);
+Console.WriteLine($"Квадрат числа {number} равен {result}"); 
+// Знак $ позволяет выводить в строке числовые переменные, нужны фигурные скобки 

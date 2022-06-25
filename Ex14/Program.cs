ea221998ffc94a78eaef96de6a88862fb1fed9d5
ELENA <elenakand@icloud.com>
2022-06-25 21:08:43 +0300

    Выведение последней цифры 3-знач числа

---
+++ Ex14/Program.cs
@@ -0,0 +1,12 @@
+﻿// На ввод трехзначное число - на вывод последняя цифра 
+Console.WriteLine("Введите трехзначное число");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine(number%10);
+}
+else 
+{
+    Console.WriteLine("Это не трехзначное число");
+}
